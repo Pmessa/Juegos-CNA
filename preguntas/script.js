49dd4e0fce30168babd649749487c53837dc233d
Pablo Messa <pablomessa@gmail>
2025-04-12 11:03:36 -0300

    nuevas preguntas agregadas

--- preguntas/script.js
+++ preguntas/script.js
@@ -1,4 +1,4 @@
-const preguntasOriginales = [
+const preguntasOriginales =[
     {
         pregunta: "SOY PERSONAL DE TURNO, ME ENCUENTRO EN ZC DE LA UNIDAD II, SUENA LA ALARMA GENERAL ¿DÓNDE ME DIRIJO?",
         opciones: ["PUNTO DE REUNIÓN N°1", "PUNTO DE REUNIÓN N°2", "PUNTO DE REUNIÓN N°3", "PUNTO DE REUNIÓN N°4", "PUNTO DE REUNIÓN N°5", "PUNTO DE REUNIÓN N°6"],
@@ -50,10 +50,151 @@ const preguntasOriginales = [
         respuesta: 1
     },
     {
-        pregunta: "1.	¿A QUE LLAMAMOS CONDICIONES METEOROLÓGICAS SEVERAS?",
+        pregunta: "¿A QUE LLAMAMOS CONDICIONES METEOROLÓGICAS SEVERAS?",
         opciones: ["A VIENTOS MAYORES A 70 KM/H", "EVENTOS ATMOSFÉRICOS CAPAZ DE PRODUCIR DIRECTA O INDIRECTAMENTE DAÑOS A PERSONAS O MATERIALES (EQUIPOS Y/O EDIFICIOS)", "SE TRATA DE SUCESOS DE ORIGEN INTERNO CUYO RESULTADO REPRESENTE UN RIESGO SIGNIFICATIVO DE LIBERACIÓN DE MATERIAL RADIACTIVO O QUÍMICO PELIGROSO","FENÓMENOS CLIMÁTICOS PELIGROSOS"],
         respuesta: 1
-    }
+    },
+        {
+          pregunta: "¿Qué es la ORE?",
+          opciones: [
+            "Es la organización del emplazamiento que debe dar respuesta ante Emergencia. Está conformada por el personal preparado, entrenado y calificado para cumplir un rol durante una situación de emergencia.",
+            "Es la Autoridad Regulatoria Nuclear.",
+            "Es el espacio físico ubicado fuera del emplazamiento a donde se trasladará el Comando de Emergencia."
+          ],
+          respuesta: 0
+        },
+        {
+          pregunta: "¿Para qué tipo de Emergencias se aplica el Plan de Emergencia?",
+          opciones: [
+            "Nuclear.",
+            "Radiológica.",
+            "Convencional."
+          ],
+          respuesta: 1
+        },
+        {
+          pregunta: "Los objetivos a cumplir durante una Emergencia son:",
+          opciones: [
+            "Proteger la salud de los trabajadores y de la comunidad.",
+            "Controlar la fuente.",
+            "Minimizar los daños sobre equipos e instalaciones.",
+            "Todas las anteriores."
+          ],
+          respuesta: 1
+        },
+          {
+          pregunta: "Cómo debe actuar frente a una Alarma General",
+          opciones: [
+            "Abandonar las tareas, y concurrir a los Puntos de Reunión.",
+            "Abandonar rápidamente el local donde se escucha la alarma utilizando las salidas habituales o de emergencia, hasta otro local donde no se escucha la alarma.",
+            "Abandonar normalmente el local donde se escucha la alarma utilizando las salidas habituales hasta otro local donde no se escucha. Hacer el cambio de ropas si sale de ZRC."
+          ],
+          respuesta: 1
+        },
+        {
+          pregunta: "Cómo debe actuar frente a una Alarma de Fuga",
+          opciones: [
+            "Abandonar las tareas, y concurrir a los Puntos de Reunión.",
+            "Abandonar rápidamente el local donde se escucha la alarma utilizando las salidas habituales o de emergencia, hasta otro local donde no se escucha la alarma.",
+            "Abandonar normalmente el local donde se escucha la alarma utilizando las salidas habituales hasta otro local donde no se escucha. Hacer el cambio de ropas si sale de ZRC."
+          ]
+        },
+        {
+          pregunta: "Cómo debe actuar frente a una alarma de Incendio:",
+          opciones: [
+            "Abandonar las tareas, y concurrir a los Puntos de Reunión.",
+            "Abandonar rápidamente el local donde se escucha la alarma utilizando las salidas habituales o de emergencia, hasta otro local donde no se escucha la alarma.",
+            "Alejarse del incendio. El personal de la Brigada de Emergencia debe acudir al lugar del evento de acuerdo al procedimiento PS-E-16."
+          ]
+        },
+        {
+          pregunta: "La Alarma General se emite cuando:",
+          opciones: [
+            "Hay probabilidad de emisión de material radioactivo al ambiente, lo que implica encontrarse en estado de Alerta Verde.",
+            "Hay probabilidad de emisión de material radioactivo al ambiente, lo que implica encontrarse en estado de Alarma Roja.",
+            "La emisión de material radioactivo al ambiente se está produciendo, lo que implica un estado de Alerta Verde.",
+             "La emisión de material radioactivo al ambiente se está produciendo, lo que implica un estado de Alerta Roja.",
+             "Se emite cuando es necesario concentrar al personal en los puntos de reunión."
+          ]
+        },
+          {
+          pregunta: "¿En una situación de emergencia la Alarma General es emitida en la etapa de pre-emisión radiactiva, por lo tanto, en dicha circunstancia se requiere del personal:",
+          opciones: [
+            "Dejar lo que está haciendo, y correr hacia los puntos de reunión.",
+            "Serenidad, dirigirse al punto de reunión correspondiente y acatar las órdenes del coordinador."
+          ]
+        },
+          {
+          pregunta: "¿Cuándo se puede retirar el personal del Punto de Reunión?",
+          opciones: [
+            "Cuando el coordinador lo determine y de la orden de abandonar el punto de reunión.",
+            "En cualquier momento, luego de haber firmado la evidencia objetiva de la capacitación."
+          ]
+        },
+        {
+          pregunta: "¿A qué Punto de Reunión debe concurrir el personal que se encuentra en la zona convencional de CNAI, que no pertenece a la ORE, al momento de declararse la Emergencia?",
+          opciones: [
+            "Punto de Reunión Nº 1 “Punto de Reunión Externo”.",
+            "Punto de Reunión Nº 2: “Sala de visita Relaciones Públicas”.",
+            "Punto de Reunión Nº 5: “CICE”."
+          ]
+        },
+        {
+          pregunta: "¿A qué Punto de Reunión debe concurrir el personal que se encuentra en la zona convencional de CNAII, que no pertenece a la ORE, al momento de declararse la Emergencia?",
+          opciones: [
+            "Punto de Reunión Nº 1 “Punto de Reunión Externo”.",
+            "Punto de Reunión Nº 4: “Oficina de Personal de Turno de la Unidad II” UMA.",
+            "Punto de Reunión Nº 6: “Edificio de Comedor”."
+          ]
+        },
+        {
+          pregunta: "¿Hacia dónde se deben dirigir contratistas, si se emite la Alarma General con la planta en Operación Normal o parada programada? ",
+          opciones: [
+            "Punto de Reunión Nº 2: “Sala de visita Relaciones Públicas”.",
+            "Punto de Reunión Nº 6: “Edificio de Comedor”.",
+            "Punto de Reunión Nº 4: “Oficina de Personal de Turno de la Unidad II” UMA."
+          ]
+        },
+        {
+          pregunta: "Ud. como miembro de la ORE estando disponible y ante la emisión de la Alarma General en la CNA I-II o al ser convocado por el Comando de Emergencia se debe dirigir a",
+          opciones: [
+            "Punto de Reunión Nº 2: “Sala de visita Relaciones Públicas”.",
+            "Punto de Reunión Nº 5: “CICE”.",
+            "Punto de Reunión Nº 1 “Punto de reunión Externo”."
+          ]
+        },
+        {
+          pregunta: "Ud. como miembro de la ORE no estando disponible y ante la emisión de la Alarma General en la CNA I-II se debe dirigir a:",
+          opciones: [
+            "Punto de Reunión Nº 2: “Sala de visita Relaciones Públicas”.",
+            "Punto de Reunión Nº 5: “CICE”.",
+            "Punto de Reunión Nº 1: “Punto de Reunión Externo”."
+          ]
+        },
+        {
+          pregunta: "Ud. como miembro de la ORE estando disponible y estando dentro de la Zona Radiológicamente Controlada, ante la emisión de la Alarma General en la CNA I-II o al ser convocado por el Comando de Emergencia se debe dirigir a",
+          opciones: [
+            "Punto de Reunión Nº 2: “Sala de visita Relaciones Públicas”.",
+            "Punto de Reunión Nº 5: “CICE”.",
+            "Punto de Reunión Nº 1 “Punto de reunión Externo”."
+          ]
+        },
+        {
+          pregunta: "En caso de ser convocado, ud. como miembro de la ORE debe presentarse en la CNA I-II en un plazo máximo de: ",
+          opciones: [
+            "60 min.",
+            "120 min.",
+            "180 min."
+          ]
+        },
+        {
+          pregunta: "Frente a la ocurrencia de un evento anormal significativo para la seguridad en un espacio/Instalación común, o no claramente definida el Jefe de la Emergencia en los primeros instantes será el:",
+          opciones: [
+            "Jefe Turno Unidad II.",
+            "Gerente.",
+            "Jefe Turno Unidad I."
+          ]
+        }
 ];
 
 let preguntas = [];
